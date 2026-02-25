50731cd8f96513fe11aa62e3990c298976ea323f
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2026-02-25 21:11:50 +0530

    feat:fixed rectangular div position

--- src/components/home/Service.jsx
+++ src/components/home/Service.jsx
@@ -7,8 +7,20 @@ const Service = () => {
   return (
 
     <div className="w-full relative py-0 flex flex-col items-end pr-3 md:pr-10">
-      <div className="w-[90%] sm:w-[85%] md:w-[65%] xl:w-[55%] h-32 sm:h-40 md:h-38 xl:h-55 border-t border-l border-b border-gray-300/40 rounded-l-xl backdrop-blur-sm -mt-8 sm:-mt-10 md:mt-5 xl:-mt-15 shadow-[-35px_15px_50px_rgba(0,0,0,0.45)] " ></div>
-      <div className="absolute top-12 md:top-4 left-1/2 -translate-x-1/2 md:left-10 md:translate-x-0 text-center md:text-left"> <h1 className="text-4xl sm:text-6xl md:text-8xl xl:text-[14rem] text-gray-400 leading-none"> Our Services </h1> <h2 className="text-md sm:text-xl md:text-3xl font-semibold text-gray-700 mt-2"> Provides A Reliable Services </h2> </div>
+      <div
+        className="
+    w-[90%] sm:w-[85%] md:w-[65%] xl:w-[55%]
+    h-32 sm:h-40 md:h-38 xl:h-55
+    rounded-l-xl
+    backdrop-blur-[2px]
+    border-t border-l border-b border-gray-300/40
+
+    mt-6 sm:mt-10 md:mt-5 xl:-mt-15
+
+    shadow-[-35px_15px_50px_rgba(0,0,0,0.45)]
+  "
+      ></div>
+      <div className="absolute top-12 md:top-4 left-1/2 -translate-x-1/2 md:left-10 md:translate-x-0 text-center md:text-left"> <h1 className="text-4xl sm:text-6xl md:text-8xl xl:text-[14rem] text-gray-400 leading-none whitespace-nowrap w-max"> Our Services </h1> <h2 className="text-md sm:text-xl md:text-3xl font-semibold text-gray-700 mt-2"> Provides A Reliable Services </h2> </div>
       <div className="p-5 md:p-13 space-y-10 w-full mt-20"> <div className="grid md:grid-cols-3 gap-8">
         <div className="group bg-white rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl transition duration-500">
           <img
